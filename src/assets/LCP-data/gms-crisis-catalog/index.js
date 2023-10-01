492b44db9b2617747d312f3224301acf26eee455
SpenTrek <spentrek0@gmail.com>
2023-10-01 18:29:47 -0400

    s1
    please god work

---
+++ src/assets/LCP-data/gms-crisis-catalog/index.js
@@ -0,0 +1,19 @@
+"use strict"
+
+import frames from './frames.json'
+import lcp_manifest from './lcp_manifest.json'
+import systems from './systems.json'
+import tables from './tables.json'
+import tags from './frames.json'
+import weapons from './weapons.json'
+
+const data = {
+  frames: frames,
+  lcp_manifest: lcp_manifest,
+  systems: systems,
+	tables: tables,
+	tags: tags,
+  weapons: weapons,
+}
+
+export default data
